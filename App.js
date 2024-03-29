2e6e0326d2e9520bff4923e3ade3cd4e3992bab9
Your Name <you@example.com>
2024-03-28 22:05:44 -0300

    Permissão Camera

---
+++ App.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType, FlashMode } from 'expo-camera';
+import { useState } from 'react';
+import ActionButton from './src/components/ActionButton/ActionButton';
+
+export default function App() {  
+  
+  const [ permission, requestPermission ] = Camera.useCameraPermissions();
+  const [ ligado, setLigado ] = useState(false);
+  
+  const handleTggleCamera = () => {
+      setLigado(!ligado);
+  }
+
+
+  if (!permission){
+    return <View><Text>Problema de permissao</Text></View>
+  }
+
+  if (!permission.granted){
+    return(
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Precisamos de acesso a sua camera</Text>
+        <Button onPress={requestPermission} title='Atribuir permissao' />
+      </View>
+    );
+  }
+
+
+  return (
+    <>
+      <View style={styles.container}>
+        <Camera
+          style={styles.camera}
+          type={CameraType.back}
+          flashMode={ligado ? FlashMode.torch : FlashMode.off}
+        />
+      </View>
+      <ActionButton
+        action={handleTggleCamera}
+        title={ligado ? 'Desligar lanterna' : 'Ligar lanterna'}
+      />      
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  camera: {
+    width: 300,
+    height: 300,
+    justifyContent:'center'
+  }
+})
